a5709e4d173eef10ec99777d4ebd82095cffafd8
atklakiant <geeniteemail@gmail.com>
2026-07-04 04:07:12 +0100

    clean up root and implement public serialize

--- src/root.zig
+++ src/root.zig
@@ -1,8 +1,17 @@
-pub const Event = @import("event.zig").Event;
-pub const Role = @import("event.zig").Role;
-pub const Context = @import("context.zig").Context;
-pub const UserId = @import("context.zig").UserId;
-pub const Server = @import("server.zig").Server;
-pub const Client = @import("client.zig").Client;
+pub const serialize = @import("serialize.zig");
 pub const compress = @import("compress.zig");
-pub const CompressionMethod = @import("compress.zig").Method;
+pub const context = @import("context.zig");
+pub const server = @import("server.zig");
+pub const client = @import("client.zig");
+pub const event = @import("event.zig");
+
+pub const CompressionMethod = compress.Method;
+
+pub const Context = context.Context;
+pub const UserId = context.UserId;
+
+pub const Server = server.Server;
+pub const Client = client.Client;
+
+pub const Event = event.Event;
+pub const Role = event.Role;
